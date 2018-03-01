507e63f850dff5e468040cf0c1e98a822c04deb2
MrFlynn <nick@pleatsikas.me>
2018-03-01 00:13:23 -0800

    Fixed port access for keyboard data bus.

--- drivers/keyboard.h
+++ drivers/keyboard.h
@@ -4,7 +4,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define DATAPORT (~PORTD & 0x02)    // Data bus.
+#define DATAPORT (~PIND & 0x02)    // Data bus.
 #define VECTORPORT INT0_vect        // Interrupt vector port.
 
 // Current character retuned by the keyboard.
