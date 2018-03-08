419f62a6c9802804c4f0c8bee5de1c8ee33a299b
MrFlynn <nick@pleatsikas.me>
2018-03-07 23:44:55 -0800

    Added several new read/write interfaces for EEPROM.

--- drivers/storage.c
+++ drivers/storage.c
@@ -1,13 +1,68 @@
 #include "storage.h"
 
-void write_char(uint8_t addr, char c) {
-    // Writes char at given address.
-    uint8_t *addr_ptr = addr; // Conversion between types.
-    eeprom_write_byte(addr_ptr, c);
+// Given op_t, this function writes the character to the given address in the 
+// operator node.
+void write_char(op_t * operator) {
+    uint8_t * addr_ptr = operator->addr;
+    eeprom_write_byte(addr_ptr, (uint8_t)operator->c);
 }
 
-char read_val(uint8_t addr) {
-    // Reads char from given address.
-    const uint8_t *addr_ptr = addr; // Conversion between types.
-    return eeprom_read_byte(addr_ptr);
+// Takes op_t and triggers a read from memory at the location specified in
+// the queue node. Once memory has been read, the applicable returnfunc is
+// run with te value taken from memory.
+void read_val(op_t * operator) {
+    // Convert address into address pointer.
+    const uint8_t * addr_ptr = operator->addr;
+    
+    // Get character from read operation and run operator returnfunc.
+    uint8_t ret = eeprom_read_byte(addr_ptr);
+    operator->returnfunc((char *)&ret);
+}
+
+// Takes head and tail for messaging queue and performs the operation 
+// specified by the head of the queue. Then it removes the message from the
+// queue.
+//
+// Operations will only be performed if the EEPROM is ready.
+void perform_storage_operation(op_t ** head, op_t ** tail) {
+    if (eeprom_is_ready() == 1) {
+        // Only run if the EEPROM is ready for reading or writing.
+        switch(operator->op_num):
+            case 0:
+                // Operation 0 is a read.
+                read_val(&head);
+                break;
+            case 1:
+                // Operation 1 is a write.
+                write_char(&head);
+                break;
+            default: break;
+
+        // Pop the head from the queue.
+        pop(head, tail);
+    }
+}
+
+// Pushes individual chracter writes on to the queue in preparation for a write
+// operation. This function assumes that the characters will be written to
+// consecutive locations in memory.
+void queue_string_write(char * string,
+    uint8_t start_addr,
+    op_t ** head,
+    op_t ** tail) {
+
+    for (uint8_t i = 0; i < strlen(string); i++) {
+        // Allocate memory for new operation.
+        op_t * new_write = malloc(sizeof(op_t));
+
+        // Setup new write operation for the queue.
+        new_write->op_num = 1;
+        new_write->addr = start_addr + i;
+        new_write->c = string[i];
+        new_write->returnfunc = &voidfunc;
+        new_write->next = NULL;
+
+        // Push on to the queue.
+        push(head, tail, new_write);
+    }
 }
--- drivers/storage.h
+++ drivers/storage.h
@@ -2,32 +2,35 @@
 #define STORAGE_H
 
 #include <avr/eeprom.h>
+#include <string.h>
+#include "../lib/queue.c"
 
 // Max number of files to store and the max filename length.
 #define MAX_FILES 16
 #define MAX_FILENAME_LENGTH 8
 
-/*
- * Region to store filenames and starting and ending addresses.
- * File information consists of 8 bytes for the filename, 1 byte for the 
- * starting address, and 1 byte for the ending address. 
- */
-#define FILE_INFO_START 0x0004
-#define FILE_INFO_END   0x0504
-
-/*
- * Holds addresses for next file info block and next place file data can be
- * stored.
- */
+// Holds addresses for next file info block and next place file data can be
+// stored.
 #define NEXT_FILE_INFO_ADDR 0x0000
-#define NEXT_FILE_BYTE_ADDR 0x5005
+#define NEXT_FILE_BYTE_ADDR 0x0001
+
+// Region to store filenames and starting and ending addresses.
+// File information consists of 8 bytes for the filename, 1 byte for the 
+// starting address, and 1 byte for the ending address. 
+#define FILE_INFO_START 0x0002
+#define FILE_INFO_END   0x00A2
 
 // Region for storing bytes of information for each file.
-#define FILE_REGION_START   0x0505
-#define FILE_REGION_END     0x8000 // 4KB max size.
+#define FILE_REGION_START 0x00A3
+#define FILE_REGION_END   0x0FA0 // 4K max number of addresses.
 
 // Functions:
-void write_char(uint8_t addr, char c);
-char read_val(uint8_t addr);
+void write_char(op_t * operator);
+void read_val(op_t * operator);
+void perform_storage_operation(op_t ** head, op_t ** tail);
+void queue_string_write(char * string, 
+    uint8_t start_addr, 
+    op_t ** head, 
+    op_t ** tail);
 
 #endif
