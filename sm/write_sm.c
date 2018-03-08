37d63a5c9cb07cc63b1c34aeb0a0f97cb3cf7d7f
MrFlynn <nick@pleatsikas.me>
2018-03-08 15:22:03 -0800

    Adding storage write state machine.

---
+++ sm/write_sm.c
@@ -0,0 +1,66 @@
+#include "write_sm.h"
+
+// Main state machine for write operations. Takes a write_state and returns
+// the next state in the code.
+WRITE_STATES write_tick(WRITE_STATES state) {
+    switch(state) { // Transitions
+        case write_start:
+            state = write_start;
+            break;
+        case write_wait:
+            // Transition to `write_queue_ops` when flag is enabled.
+            state = write_flag == 0x01 ? write_queue_ops : write_wait;
+            break;
+        case write_queue_ops:
+            state = write_run_ops;
+            break;
+        case write_run_ops:
+            if (empty(write_head, write_tail)) {
+                // If the queue is empty, go back to wait and reset flag.
+                state = write_wait;
+                write_flag = 0x00;
+            } else {
+                state = write_run_ops;
+            }
+            break;
+        default:
+            state = write_start;
+            break;
+    }
+
+    switch(state) { // Actions
+        case write_start: break;
+        case write_wait: break;
+        case write_queue_ops:
+            // Add filename and file contents strings into messaging queue.
+            queue_string_write(write_fname, 
+                next_file_info_addr, 
+                write_head,
+                write_tail);
+            queue_string_write(write_string,
+                next_file_data_addr,
+                write_head,
+                write_tail);
+            
+            uint8_t string_length = strlen(write_string);
+
+            // Increment location for next string writes.
+            next_file_data_addr += string_length;
+
+            // Increment location for file info.
+            next_file_info_addr += 8;
+
+            // TODO: Write more file info here.
+            // i.e. next_file_info_addr, next_file_info_addr, and string
+            // bounding locations into memory.
+
+            break;
+        case write_run_ops:
+            // Keep performing operations and popping them off the list.
+            perform_storage_operation(write_head, write_tail);
+            break;
+        default: break;
+    }
+
+    return state;
+}
