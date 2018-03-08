c0a9a10d1bbb7f15b3730acfe390b8a9f737ce6c
MrFlynn <nick@pleatsikas.me>
2018-03-08 15:21:17 -0800

    Adding common variables header.

---
+++ common.h
@@ -0,0 +1,26 @@
+// Header for shared variables and other arrays that need to be accessed by
+// multiple state machines.
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include "drivers/storage.h"
+
+// Addresses for next open file info and data blocks.
+uint8_t next_file_info_addr = 0x00;
+uint8_t next_file_data_addr = 0x00;
+
+// Cached list of files and memory indexes.
+char * file_list[MAX_FILES];
+char * file_addr_indexes[MAX_FILES][2];
+uint8_t num_files = 0x00;
+
+// Locks for performing reading and writing.
+uint8_t write_flag = 0x00;
+uint8_t read_flag = 0x00;
+
+// Data to write section.
+char * write_fname[MAX_FILENAME_LENGTH];
+char * write_string[MAX_FILE_SIZE];
+
+#endif
