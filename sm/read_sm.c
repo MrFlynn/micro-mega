a3a0c5a8e4c7c3bd6f378e357f913c4fc55b7e2e
MrFlynn <nick@pleatsikas.me>
2018-03-10 23:39:30 -0800

    Adding data reading state machine.

---
+++ sm/read_sm.c
@@ -0,0 +1,46 @@
+#include "read_sm.h"
+
+enum READ_STATES read_tick(enum READ_STATES state) {
+    switch(state) { // Transitions
+        case read_start:
+            state = read_wait;
+            break;
+        case read_wait:
+            // Go to next state if the flag to read is set.
+            state = read_flag == 0x01 ? read_queue_ops : read_wait;
+            break;
+        case read_queue_ops:
+            state = read_run_ops;
+            break;
+        case read_run_ops:
+            // Reset flag and go back to wait if read queue is empty.
+            if (empty(&read_head, &read_tail)) {
+                state = read_wait;
+                read_flag = 0x00;
+            } else {
+                state = read_run_ops;
+            }
+
+            break;
+        default:
+            state = read_start;
+            break;
+    }
+
+    switch(state) { // Actions
+        case read_start: break;
+        case read_wait: break;
+        case read_queue_ops:
+            // Queue data read operations.
+            read_in_range(read_start_addr, offset, &read_head, &read_tail);
+            break;
+        case write_run_ops:
+            // Continuously perform operations until queue is empty.
+            perform_storage_operation(&read_head, &read_tail);
+            break;
+        default:
+            break;
+    }
+
+    return state;
+}
---
+++ sm/read_sm.h
@@ -0,0 +1,20 @@
+#ifndef READ_SM
+#define READ_SM
+
+#include <string.h>
+
+#include "../common.h"
+#include "../lib/queue.h"
+#include "../drivers/storage.c"
+
+typedef struct READ_STATES {
+    read_start,
+    read_wait,
+    read_queue_ops,
+    read_run_ops
+} read_state;
+
+op_t * read_head = NULL;
+op_t * read_tail = NULL;
+
+#endif
