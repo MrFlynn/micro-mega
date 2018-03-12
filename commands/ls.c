9dcfaada9886e74cfa8d92f662ed07316c72de1c
MrFlynn <nick@pleatsikas.me>
2018-03-11 23:54:36 -0700

    Getting proper size of object.

--- commands/ls.c
+++ commands/ls.c
@@ -3,7 +3,7 @@
 
 void ls_command() {
     for (uint8_t i = 0; i < num_files; i++) {
-        for (uint8_t j = 0; i < sizeof(file_list); i++) {
+        for (uint8_t j = 0; i < sizeof(file_list[i]); i++) {
             strcat(disp_buffer, (char *)file_list[i][j]);
         }
 
