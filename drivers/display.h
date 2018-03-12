25dbe41d52a31e7013729c45d77f0400fffb0bac
MrFlynn <nick@pleatsikas.me>
2018-03-11 22:33:43 -0700

    Adding mainline display drivers.

---
+++ drivers/display.h
@@ -0,0 +1,10 @@
+#ifndef __display_h__
+#define __display_h__
+
+void LCD_init();
+void LCD_ClearScreen(void);
+void LCD_WriteCommand (unsigned char Command);
+void LCD_Cursor (unsigned char column);
+void LCD_DisplayString(unsigned char column ,const unsigned char *string);
+void delay_ms(int miliSec);
+#endif
