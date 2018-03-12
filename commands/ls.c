230e972d7a35ff207079aa366cd506ea769d4229
MrFlynn <nick@pleatsikas.me>
2018-03-11 22:33:10 -0700

    Fixed some compile-time bugs in commands.

--- commands/ls.c
+++ commands/ls.c
@@ -1,9 +1,10 @@
 #include <string.h>
+#include "../common.h"
 
 void ls_command() {
     for (uint8_t i = 0; i < num_files; i++) {
-        for (uint8_t j = 0; i < strlen()) {
-            strcat(disp_buffer, file_list[i][j]);
+        for (uint8_t j = 0; i < sizeof(file_list); i++) {
+            strcat(disp_buffer, (char *)file_list[i][j]);
         }
 
         strcat(disp_buffer, " ");
